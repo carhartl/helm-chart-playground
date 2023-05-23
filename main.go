4d1d506d8613fc3a41eb007062203e1bea70e5d4
Klaus Hartl <klaus.hartl@gmail.com>
2023-05-23 18:59:43 +0200

    Implement accessing pods in cluster

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	for {
-		fmt.Println("Housekeeping...")
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Second)
+
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 	}
 }
