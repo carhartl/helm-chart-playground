2721f447ca1a177a47e62ad8b12c8c7c23147fa1
Klaus Hartl <klaus.hartl@gmail.com>
2023-05-24 16:16:26 +0200

    Implement rule evaluation for pods
    Assumptions:
    - Not part of the evaluation are pods in the `kube-system` as well as
      our own `housekeeping` namespace
    - Init containers are not to be evaluated

--- main.go
+++ main.go
@@ -2,13 +2,56 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
+
+type Rule struct {
+	Name  string `json:"name"`
+	Valid bool   `json:"valid"`
+}
+
+type PodEvaluationResult struct {
+	Pod            string `json:"pod"`
+	RuleEvaluation []Rule `json:"rule_evaluation"`
+}
+
+func evaluateImagePrefix(pod corev1.Pod) Rule {
+	valid := true
+	// NOTE: for now ignoring init containers!
+	for _, container := range pod.Spec.Containers {
+		if !strings.HasPrefix(container.Image, "bitnami/") {
+			valid = false
+			break
+		}
+	}
+	return Rule{Name: "image_prefix", Valid: valid}
+}
+
+func evaluateTeamLabel(pod corev1.Pod) Rule {
+	valid := len(pod.Labels["team"]) > 0
+	return Rule{Name: "team_label_present", Valid: valid}
+}
+
+func evaluateStartTime(pod corev1.Pod) Rule {
+	valid := pod.GetCreationTimestamp().Time.After(time.Now().Add(-7 * 24 * time.Hour))
+	return Rule{Name: "recent_start_time", Valid: valid}
+}
+
+func evaluatePodCompliance(pod corev1.Pod) PodEvaluationResult {
+	return PodEvaluationResult{Pod: pod.Name, RuleEvaluation: []Rule{
+		evaluateImagePrefix(pod),
+		evaluateTeamLabel(pod),
+		evaluateStartTime(pod),
+	}}
+}
 
 func main() {
 	config, err := rest.InClusterConfig()
@@ -27,6 +70,15 @@ func main() {
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		for _, pod := range pods.Items {
+			if pod.Namespace == "kube-system" || pod.Namespace == "housekeeping" {
+				continue
+			}
+			if pod.Status.Phase == "Running" {
+				r := evaluatePodCompliance(pod)
+				bytes, _ := json.Marshal(r)
+				fmt.Println(string(bytes))
+			}
+		}
 	}
 }
