f896e2a3fc743ffc126cdfbc40a39de151a6c90b
Klaus Hartl <klaus.hartl@gmail.com>
2023-06-15 15:52:38 +0200

    Add integration test for pod security enforcement
    - Adding an if condition around Clusterrole resources to avoid error
      when encountering already existing objects (either from earlier test
      or by dev on the same cluster).
    - Prettier (and yamllint) can't handle
    
    One solution to the first might be to use the `helm.sh/resource-policy:
    keep` annotation? The current workaround relies on `helm template` being
    executed as part of the installation..

--- test/deployment_integration_test.go
+++ test/deployment_integration_test.go
@@ -38,3 +38,44 @@ func TestDeployment(t *testing.T) {
 	sleep := 2 * time.Second
 	k8s.WaitUntilDeploymentAvailable(t, kubectlOptions, "housekeeping", retries, sleep)
 }
+
+func TestNamespaceLabels(t *testing.T) {
+	helmChartPath := "../charts/housekeeping"
+	testNamespace := fmt.Sprintf("housekeeping-test-%s", strings.ToLower(random.UniqueId()))
+	releaseName := fmt.Sprintf("test-%s", strings.ToLower(random.UniqueId()))
+
+	helmOptions := &helm.Options{
+		SetValues: map[string]string{
+			"image.repository": "ghcr.io/carhartl/cluster-housekeeping/housekeeping",
+			"image.tag":        "latest",
+			"image.pullPolicy": "IfNotPresent"},
+		ExtraArgs: map[string][]string{
+			"install": []string{"--namespace", testNamespace, "--create-namespace"},
+			"delete":  []string{"--namespace", testNamespace},
+		},
+	}
+	helm.Install(t, helmOptions, helmChartPath, releaseName)
+	defer helm.Delete(t, helmOptions, releaseName, true)
+
+	// Now that the chart is deployed, verify the deployment.
+	// Setup the kubectl config and context. Here we choose to use the defaults, which is:
+	// - HOME/.kube/config for the kubectl config file
+	// - Current context of the kubectl config file
+	kubectlOptions := k8s.NewKubectlOptions("", "", testNamespace)
+
+	ns := k8s.GetNamespace(t, kubectlOptions, testNamespace)
+	labels := ns.Labels
+
+	var label string
+	var ok bool
+	if label, ok = labels["pod-security.kubernetes.io/enforce"]; ok {
+		if label != "restricted" {
+			t.Fatalf("Expected label \"pod-security.kubernetes.io/enforce\" to have value \"restricted\", got %s", label)
+		}
+	}
+	if label, ok = labels["pod-security.kubernetes.io/enforce-version"]; ok {
+		if label != "latest" {
+			t.Fatalf("Expected label \"pod-security.kubernetes.io/enforce-version\" to have value \"latest\", got %s", label)
+		}
+	}
+}
