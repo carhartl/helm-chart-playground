cedda2cd11e3b9e790c714116c2939adf5e1ad52
Klaus Hartl <klaus.hartl@gmail.com>
2023-06-19 16:54:07 +0200

    Introduce testify to streamline test setup
    Both in terms of time it takes to run tests as well as writing;
    eliminating repetition SonarQube complains about..

--- test/deployment_integration_test.go
+++ test/deployment_integration_test.go
@@ -11,51 +11,39 @@ import (
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestDeployment(t *testing.T) {
-	docker.Build(t, "../.", &docker.BuildOptions{
-		Tags: []string{"housekeeping:test"},
-		Env: map[string]string{
-			"DOCKER_TLS_VERIFY":       os.Getenv("DOCKER_TLS_VERIFY"),
-			"DOCKER_HOST":             os.Getenv("DOCKER_HOST"),
-			"DOCKER_CERT_PATH":        os.Getenv("DOCKER_CERT_PATH"),
-			"MINIKUBE_ACTIVE_DOCKERD": os.Getenv("MINIKUBE_ACTIVE_DOCKERD"),
-		},
-	})
+var (
+	helmChartPath = "../charts/housekeeping"
+	testNamespace = fmt.Sprintf("housekeeping-test-%s", strings.ToLower(random.UniqueId()))
+	releaseName   = fmt.Sprintf("test-%s", strings.ToLower(random.UniqueId()))
 
-	helmChartPath := "../charts/housekeeping"
-	testNamespace := fmt.Sprintf("housekeeping-test-%s", strings.ToLower(random.UniqueId()))
-	releaseName := fmt.Sprintf("test-%s", strings.ToLower(random.UniqueId()))
-
-	helmOptions := &helm.Options{
+	helmOptions = &helm.Options{
 		SetValues: map[string]string{
 			"image.repository": "housekeeping",
 			"image.tag":        "test",
 			"image.pullPolicy": "Never",
 		},
 		ExtraArgs: map[string][]string{
-			"install": []string{"--namespace", testNamespace, "--create-namespace"},
-			"delete":  []string{"--namespace", testNamespace},
+			"install": {"--namespace", testNamespace, "--create-namespace"},
+			"delete":  {"--namespace", testNamespace},
 		},
 	}
+
 	// Now that the chart is deployed, verify the deployment.
 	// Setup the kubectl config and context. Here we choose to use the defaults, which is:
 	// - HOME/.kube/config for the kubectl config file
 	// - Current context of the kubectl config file
-	kubectlOptions := k8s.NewKubectlOptions("", "", testNamespace)
+	kubectlOptions = k8s.NewKubectlOptions("", "", testNamespace)
+)
 
-	helm.Install(t, helmOptions, helmChartPath, releaseName)
-	defer helm.Delete(t, helmOptions, releaseName, true)
-	defer k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
-
-	retries := 5
-	sleep := 2 * time.Second
-	k8s.WaitUntilDeploymentAvailable(t, kubectlOptions, "housekeeping", retries, sleep)
+type Suite struct {
+	suite.Suite
 }
 
-func TestNamespaceLabels(t *testing.T) {
-	docker.Build(t, "../.", &docker.BuildOptions{
+func (s *Suite) SetupSuite() {
+	docker.Build(s.T(), "../.", &docker.BuildOptions{
 		Tags: []string{"housekeeping:test"},
 		Env: map[string]string{
 			"DOCKER_TLS_VERIFY":       os.Getenv("DOCKER_TLS_VERIFY"),
@@ -64,44 +52,40 @@ func TestNamespaceLabels(t *testing.T) {
 			"MINIKUBE_ACTIVE_DOCKERD": os.Getenv("MINIKUBE_ACTIVE_DOCKERD"),
 		},
 	})
+	helm.Install(s.T(), helmOptions, helmChartPath, releaseName)
+}
 
-	helmChartPath := "../charts/housekeeping"
-	testNamespace := fmt.Sprintf("housekeeping-test-%s", strings.ToLower(random.UniqueId()))
-	releaseName := fmt.Sprintf("test-%s", strings.ToLower(random.UniqueId()))
+func (s *Suite) TearDownSuite() {
+	helm.Delete(s.T(), helmOptions, releaseName, true)
+	k8s.DeleteNamespace(s.T(), kubectlOptions, testNamespace)
+	docker.DeleteImage(s.T(), "housekeeping:test", nil)
+}
 
-	helmOptions := &helm.Options{
-		SetValues: map[string]string{
-			"image.repository": "housekeeping",
-			"image.tag":        "test",
-			"image.pullPolicy": "Never",
-		},
-		ExtraArgs: map[string][]string{
-			"install": []string{"--namespace", testNamespace, "--create-namespace"},
-			"delete":  []string{"--namespace", testNamespace},
-		},
-	}
-	// Now that the chart is deployed, verify the deployment.
-	// Setup the kubectl config and context. Here we choose to use the defaults, which is:
-	// - HOME/.kube/config for the kubectl config file
-	// - Current context of the kubectl config file
-	kubectlOptions := k8s.NewKubectlOptions("", "", testNamespace)
+func (s *Suite) TestDeployment() {
+	retries := 5
+	sleep := 2 * time.Second
+	k8s.WaitUntilDeploymentAvailable(s.T(), kubectlOptions, "housekeeping", retries, sleep)
+}
 
-	helm.Install(t, helmOptions, helmChartPath, releaseName)
-	defer helm.Delete(t, helmOptions, releaseName, true)
-	defer k8s.DeleteNamespace(t, kubectlOptions, testNamespace)
-
-	ns := k8s.GetNamespace(t, kubectlOptions, testNamespace)
+func (s *Suite) TestNamespaceLabels() {
+	ns := k8s.GetNamespace(s.T(), kubectlOptions, testNamespace)
 	labels := ns.Labels
-	var label string
-	var ok bool
+	var (
+		label string
+		ok    bool
+	)
 	if label, ok = labels["pod-security.kubernetes.io/enforce"]; ok {
 		if label != "restricted" {
-			t.Fatalf("Expected label \"pod-security.kubernetes.io/enforce\" to have value \"restricted\", got %s", label)
+			s.T().Fatalf("Expected label \"pod-security.kubernetes.io/enforce\" to have value \"restricted\", got %s", label)
 		}
 	}
 	if label, ok = labels["pod-security.kubernetes.io/enforce-version"]; ok {
 		if label != "latest" {
-			t.Fatalf("Expected label \"pod-security.kubernetes.io/enforce-version\" to have value \"latest\", got %s", label)
+			s.T().Fatalf("Expected label \"pod-security.kubernetes.io/enforce-version\" to have value \"latest\", got %s", label)
 		}
 	}
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(Suite))
 }
--- test/go.mod
+++ test/go.mod
@@ -45,7 +45,8 @@ require (
 	github.com/pquerna/otp v1.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/urfave/cli v1.22.2 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
